c4836fc37cfea616b4716fed13c38e81d9374772
MatviyEgorov23 <matviy.egorov@gmail.com>
2023-11-08 21:44:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	int sum = 0;
+	cout << "Enter the number: ";
+	cin >> x;
+	for (; x > 0;) {
+		y = x % 10;
+		sum +=y;
+		x /= 10;
+	}
+	cout << sum << endl;
+	return 0;
+}
